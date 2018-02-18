cfef28a00eeb451aa07a73d7e264188caf0005ee
Cecily <cecily.downs@gmail.com>
2018-02-17 18:25:41 -0800

    Set up components for each page

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => (
+  <div>
+      About Page Content
+  </div>
+)
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => (
+  <div>
+      Home Page Content
+  </div>
+)
+
+export default Home
